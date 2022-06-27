0f72d932c066647dcc360e3c04fdd061c67acc07
Sully <msullivan@scoir.com>
2022-06-27 17:08:26 -0400

    Method extract refactor

--- nested_document_array/querying_test.go
+++ nested_document_array/querying_test.go
@@ -8,35 +8,46 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"strconv"
+	"time"
 )
+
+func generateNestedDocumentArray(ctx context.Context, count int) (result *mongo.InsertOneResult, err error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	database := client.Database(DocumentStoreModelingDatabaseName)
+	collection := database.Collection(NestedDocumentArrayCollectionName)
+	err = collection.Drop(ctx)
+	if err != nil {
+		return result, err
+	}
+
+	sampleDocument := &SampleDocument{}
+	for i := 1; i <= count; i++ {
+		sampleNestedDocument := &SampleNestedDocument{
+			Name:    "Test Name " + strconv.Itoa(i),
+			Phone:   strconv.Itoa(rangeIn(1000000000, 9999999999)),
+			Address: strconv.Itoa(rangeIn(1000, 9999)) + " Street",
+		}
+		sampleDocument.NestedDocuments = append(sampleDocument.NestedDocuments, sampleNestedDocument)
+	}
+
+	result, err = collection.InsertOne(ctx, sampleDocument)
+	return result, err
+}
 
 var _ = Describe("Nested Document Array", func() {
 	FContext("Query by Name", func() {
 		It("Client-side query", func() {
-			ctx := context.TODO()
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			result, err := generateNestedDocumentArray(ctx, 100)
+			Expect(err).To(BeNil())
+
 			client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 			Expect(err).To(BeNil())
+			collection := client.
+				Database(DocumentStoreModelingDatabaseName).
+				Collection(NestedDocumentArrayCollectionName)
 
-			database := client.Database(DocumentStoreModelingDatabaseName)
-			collection := database.Collection(NestedDocumentArrayCollectionName)
-			err = collection.Drop(ctx)
-			Expect(err).To(BeNil())
-
-			sampleDocument := &SampleDocument{}
-			count := 100
-			for i := 1; i <= count; i++ {
-				sampleNestedDocument := &SampleNestedDocument{
-					Name:    "Test Name " + strconv.Itoa(i),
-					Phone:   strconv.Itoa(rangeIn(1000000000, 9999999999)),
-					Address: strconv.Itoa(rangeIn(1000, 9999)) + " Street",
-				}
-				sampleDocument.NestedDocuments = append(sampleDocument.NestedDocuments, sampleNestedDocument)
-			}
-
-			result, err := collection.InsertOne(ctx, sampleDocument)
-			Expect(err).To(BeNil())
-
-			// Query by Name
 			findOneResult := collection.FindOne(ctx, bson.M{"_id": result.InsertedID})
 			Expect(findOneResult.Err()).To(BeNil())
 
